39a8fa1470029979b2ea173f4040bfce6957e836
Richard Alam <ritzalam@gmail.com>
2018-10-05 19:31:05 +0000

    - chapter 3

---
+++ metex/lib/metex.ex
@@ -0,0 +1,15 @@
+defmodule Metex do
+  @moduledoc """
+  Documentation for Metex.
+  """
+
+ 
+  def temperatures_of(cities) do
+    coordinator_pid = spawn(Metex.Coordinator, :loop, [[], Enum.count(cities)])
+
+    cities |> Enum.each(fn city ->
+      worker_pid = spawn(Metex.Worker, :loop, [])
+      send worker_pid, {coordinator_pid, city}
+    end)
+  end
+end
